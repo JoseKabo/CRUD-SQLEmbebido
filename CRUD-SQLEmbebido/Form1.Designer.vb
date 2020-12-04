46ca7e446faf975c696342d96ddbe5877162f111
Jose Kabo <kabo.0699@gmail.com>
2020-12-04 06:17:12 -0600

    Deleting and editing rows on sql server with vba

--- CRUD-SQLEmbebido/Form1.Designer.vb
+++ CRUD-SQLEmbebido/Form1.Designer.vb
@@ -22,17 +22,93 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.dgv_prueba = New System.Windows.Forms.DataGridView()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        CType(Me.dgv_prueba, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(107, Byte), Integer), CType(CType(107, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(781, 172)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Poppins", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.Label2.Location = New System.Drawing.Point(43, 73)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(101, 23)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Demostración"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Poppins", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(39, 85)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(214, 48)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "SQL Embebido"
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.dgv_prueba)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel2.Location = New System.Drawing.Point(0, 172)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(781, 297)
+        Me.Panel2.TabIndex = 1
+        '
+        'dgv_prueba
+        '
+        Me.dgv_prueba.BackgroundColor = System.Drawing.Color.White
+        Me.dgv_prueba.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.dgv_prueba.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal
+        Me.dgv_prueba.ColumnHeadersHeight = 25
+        Me.dgv_prueba.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+        Me.dgv_prueba.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgv_prueba.Location = New System.Drawing.Point(0, 0)
+        Me.dgv_prueba.Name = "dgv_prueba"
+        Me.dgv_prueba.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgv_prueba.Size = New System.Drawing.Size(781, 297)
+        Me.dgv_prueba.TabIndex = 0
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(781, 469)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
         Me.Name = "Form1"
+        Me.ShowIcon = False
         Me.Text = "SQLEMbebido"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        CType(Me.dgv_prueba, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents dgv_prueba As DataGridView
 End Class
--- CRUD-SQLEmbebido/Form1.vb
+++ CRUD-SQLEmbebido/Form1.vb
@@ -1,3 +1,69 @@
-﻿Public Class Form1
+﻿Imports System.Data.SqlClient
 
+Public Class Form1
+
+    Dim connection As New SqlConnection(My.Settings.connectiontoSQL)
+    Dim id As String = ""
+    Dim column As String = ""
+    Dim querySQL As String = ""
+    Dim cmd As New SqlCommand()
+    Dim dAdapter As New SqlDataAdapter()
+    Dim dSet As New DataSet()
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.querySQL = "select * from gestores_bd"
+        Me.cmd = New SqlCommand(querySQL, connection)
+        Try
+            Me.dAdapter = New SqlDataAdapter(cmd)
+            Me.dSet = New DataSet
+            dAdapter.Fill(dSet, "gestores_bd")
+            Me.dgv_prueba.DataSource = dSet.Tables("gestores_bd")
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub dgv_prueba_CellEndEdit(sender As Object, e As DataGridViewCellEventArgs) Handles dgv_prueba.CellEndEdit
+        If (Me.dgv_prueba.CurrentRow.IsNewRow = False) Then
+            Me.connection.Open()
+            Me.querySQL = "UPDATE gestores_bd SET " + Me.column + "=" & "'" + Me.dgv_prueba(e.ColumnIndex, e.RowIndex).Value.ToString() & "'" + " WHERE id=" + Me.id
+            MessageBox.Show(Me.querySQL)
+            Me.cmd = New SqlCommand(querySQL, Me.connection)
+            Try
+                Dim cant As Integer = cmd.ExecuteNonQuery()
+                If cant > 0 Then
+                    MessageBox.Show("Se han actualizado el registro")
+                End If
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+            End Try
+            Me.connection.Close()
+        End If
+    End Sub
+
+    Private Sub dgv_prueba_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgv_prueba.CellClick
+        Me.column = Me.dgv_prueba.Columns(Me.dgv_prueba.CurrentCell.ColumnIndex).Name.ToString()
+    End Sub
+
+    Private Sub dgv_prueba_UserDeletedRow(sender As Object, e As DataGridViewRowEventArgs) Handles dgv_prueba.UserDeletedRow
+        Me.connection.Open()
+        Me.querySQL = "DELETE FROM gestores_bd WHERE id =" + Me.id
+        MessageBox.Show(Me.querySQL)
+        Me.cmd = New SqlCommand(querySQL, Me.connection)
+        Try
+            Dim cant As Integer = cmd.ExecuteNonQuery()
+            If cant > 0 Then
+                MessageBox.Show("Se ha eliminado el registro")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+        Me.connection.Close()
+    End Sub
+
+
+    Private Sub dgv_prueba_SelectionChanged(sender As Object, e As EventArgs) Handles dgv_prueba.SelectionChanged
+        If (Me.dgv_prueba.CurrentRow.IsNewRow() = False) Then
+            Me.id = Me.dgv_prueba.CurrentRow.DataBoundItem("id")
+        End If
+    End Sub
 End Class
